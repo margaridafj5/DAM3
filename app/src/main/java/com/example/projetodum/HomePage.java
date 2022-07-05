90c04646415213d5122d701547f3372dc2c679c5
margaridafj5 <85556138+margaridafj5@users.noreply.github.com>
2022-07-05 18:57:27 +0000

    34456

---
+++ app/src/main/java/com/example/projetodum/HomePage.java
@@ -0,0 +1,24 @@
+<<<<<<< Updated upstream
+=======
+package com.example.projetodum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+
+
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+    }
+}
+>>>>>>> Stashed changes
